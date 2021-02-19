076db9d94c92733a6f2c8649401898ecff3015e2
John Hayes <lifefullofchange@gmail.com>
2021-02-19 13:55:38 +0300

    chore(eslint): added prettier to eslint configuration
    ESLint accepts basic prettier recommended rules and additionally will accept including .pretterrc.js
    specific rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,9 +22,11 @@ module.exports = {
     'plugin:react/recommended', // React rules
     'plugin:react-hooks/recommended', // React hooks rules
     'plugin:jsx-a11y/recommended', // Accessibility rules
+    'plugin:prettier/recommended', // Prettier rules
   ],
   rules: {
     'react/react-in-jsx-scope': 'off', // React import is not needed when using Next.js
     'jsx-a11y/anchor-is-valid': 'off', // This rule is not compatible with Next.js's <Link /> components
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }], // Include .prettierrc.js rules
   },
 }
