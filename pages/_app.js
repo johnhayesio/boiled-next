829176138e32c408ffaffc726eb7659964c167e8
John Hayes <lifefullofchange@gmail.com>
2021-02-23 15:42:37 +0300

    feat(app): added global styling
    GlobalStyles was applied to custom _app.js which includes a modern css reset. This reset has a base
    font-size of 10px rather than the standard 16px.

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 // Import packages
 import PropTypes from 'prop-types'
 
+// Import styles
+import { GlobalStyles } from '../components/GlobalStyles'
+
 const MyApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyles />
+    </>
+  )
 }
 
 // Declare prop type validation
