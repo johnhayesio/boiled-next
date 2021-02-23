0243ae019ec9dd73d82857b28094b439b101be2f
John Hayes <lifefullofchange@gmail.com>
2021-02-23 16:55:05 +0300

    style(colors): added primary and social media colors
    A consts folder has been added with all exports to be included in index.js. This commit has
    site-wide colors including primary and social media colors. Exports are not default and must be
    destrucutred.

---
+++ consts/colors.js
@@ -0,0 +1,23 @@
+// Primary Colors
+export const primary = '#458588'
+export const secondary = '#888888'
+export const success = '#98971A'
+export const danger = '#CC241D'
+export const warning = '#D79921'
+export const info = '#B16286'
+export const light = '#F3F3F3'
+export const medium = '#D9D9D9'
+export const dark = '#282828'
+export const white = '#FFFFFF'
+export const black = '#000000'
+
+// Social Media Colors
+export const social = {
+  google: '#EA4335',
+  facebook: '#3B5998',
+  twitter: '#00ACED',
+  instagram: '#3F729B',
+  linkedin: '#0E76A8',
+  youtube: '#c4302b',
+  website: '#FF5700',
+}
---
+++ consts/index.js
@@ -0,0 +1,2 @@
+// Export all consts
+export * from './colors'
