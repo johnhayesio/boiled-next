5b71c37dfea78d0b28195cc96bae7b1dad7e96cf
John Hayes <lifefullofchange@gmail.com>
2021-02-23 16:00:20 +0300

    feat(header): added header component
    A simple header component was added as an alternative to a full navbar for this boilerplate. If you
    wish to use a Navbar instead, create the component within the
    components folder.

---
+++ components/Header.js
@@ -0,0 +1,33 @@
+// Import packages
+import styled from '@emotion/styled'
+
+// Import commons
+import { Container } from './common'
+
+// Import consts
+import { medium } from '../consts'
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <StyledContainer>
+        <h1>Next SpaceX Launch</h1>
+      </StyledContainer>
+    </StyledHeader>
+  )
+}
+
+// Component styles
+const StyledHeader = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  background: ${medium};
+`
+
+const StyledContainer = styled(Container)`
+  padding: 1.5rem 3rem;
+`
+
+export default Header
