d68b21dad616bc8d0532c8576668789011034146
John Hayes <lifefullofchange@gmail.com>
2021-02-23 17:32:53 +0300

    feat(container): added styled container common
    This commit includes a common folder that has a styled container. All styled components/constants
    should be placed within the common folder. Styles to be applied to components can be added within a
    theme folder at a later time.

---
+++ components/common/Container.js
@@ -0,0 +1,26 @@
+// Import packages
+import PropTypes from 'prop-types'
+
+export const Container = ({ children }) => (
+  <>
+    <div className="container">
+      {children}
+
+      <style jsx>
+        {`
+          .container {
+            max-width: 120rem;
+            margin: 0 auto;
+            padding: 3rem;
+            overflow: auto;
+          }
+        `}
+      </style>
+    </div>
+  </>
+)
+
+// Declare prop type validation
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+}
---
+++ components/common/index.js
@@ -0,0 +1,2 @@
+// Export all commons
+export * from './Container'
