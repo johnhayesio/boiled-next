3f5a63f79f9ca9a20cf4f2c1a57d49c5f9b8550f
John Hayes <lifefullofchange@gmail.com>
2021-02-27 23:30:13 +0300

    feat(tailwindcss): added container to global styles
    Added a custom container to GlobalStyles for twin.macro/Tailwind CSS.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        maxWidth: '120rem',
+        margin: '0 auto',
+        padding: '3rem',
+        overflow: 'hidden',
+      },
+    },
   },
   variants: {
     extend: {},
