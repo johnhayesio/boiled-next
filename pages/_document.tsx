7f3dffa7d9a45903e771b7cb03e34ca12013d4a0
John Hayes <lifefullofchange@gmail.com>
2021-03-01 02:32:09 +0300

    refactor(document): remove render
    All Next.js imports for render removed. They can be added in at a later time if needed.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,29 +1,11 @@
 // Import packages
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-  DocumentInitialProps,
-} from 'next/document'
+import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
 
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
+    return { ...initialProps }
   }
 }
 
