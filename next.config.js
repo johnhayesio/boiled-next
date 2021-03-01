b1b39f236f2916889563dc62b97b8acd0a56a5f5
John Hayes <lifefullofchange@gmail.com>
2021-03-01 03:24:29 +0300

    chore(next): change aliases in next config
    Removed common alias and added pages.

--- next.config.js
+++ next.config.js
@@ -9,8 +9,8 @@ module.exports = {
     }
 
     config.resolve.alias['components'] = path.join(__dirname, 'components')
-    config.resolve.alias['common'] = path.join(__dirname, 'components', 'common')
     config.resolve.alias['consts'] = path.join(__dirname, 'consts')
+    config.resolve.alias['pages'] = path.join(__dirname, 'pages')
 
     return config
   },
