6e1f25692829f34db0adad08fb54ad06d42c5077
John Hayes <lifefullofchange@gmail.com>
2021-03-01 03:09:31 +0300

    style(container): change container max-width
    The Container component max-width is changed to 128rem or 1280px.

--- components/common/Container.js
+++ components/common/Container.js
@@ -3,7 +3,7 @@ import styled from '@emotion/styled'
 
 // Component Styles
 export const Container = styled.div`
-  max-width: 120rem;
+  max-width: 128rem;
   margin: 0 auto;
   padding: 3rem;
   overflow: auto;
