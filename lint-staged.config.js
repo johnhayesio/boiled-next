1f9e3bbef69764c214e06ee69b985d959c1508f5
John Hayes <lifefullofchange@gmail.com>
2021-03-04 02:50:55 +0300

    chore(eslint): add typescript support for eslint
    TypeScript support for ESLint was added. The lint script in package.json and lint-staged.config.js
    were updated to include TypeScript files.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  // Run check-types on changes to TypeScript files
+  '**/*.ts?(x)': () => 'yarn check-types',
   // Run eslint and prettier on changes to JavaScript files
-  '**/*.js?(x)': [filenames => `yarn lint ${filenames.join(' ')}`, 'yarn format'],
+  '**/*.(ts|js)?(x)': [filenames => `yarn lint ${filenames.join(' ')}`, 'yarn format'],
 }
