cb9df9e628b459d613d5bf89b631d1d9b24e46bb
John Hayes <lifefullofchange@gmail.com>
2021-03-06 04:08:01 +0300

    style(app): fix font-size
    Fix font-size of index.js and Header.js after removing global font-size of 62.5% or 10px.

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,8 @@ import tw, { css, styled, theme } from 'twin.macro' // eslint-disable-line
 const Header = () => {
   return (
     <div tw="fixed top-0 left-0 w-full bg-gray-200">
-      <div css={css([theme`container`, tw`py-6`])}>
-        <h1 tw="text-3xl">Next SpaceX Launch</h1>
+      <div css={css([theme`container`, tw`py-3`])}>
+        <h1 tw="text-xl">Next SpaceX Launch</h1>
       </div>
     </div>
   )
